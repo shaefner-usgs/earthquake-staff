bbb687fe1fd4ea7212eb00565e7d1ea415bf8703
Scott Haefner <shaefner@usgs.gov>
2017-04-26 15:57:10 -0700

    add method to get full name

--- src/lib/classes/Employee.class.php
+++ src/lib/classes/Employee.class.php
@@ -23,4 +23,21 @@ class Employee {
   public function getFirstLetter () {
     return strtoupper(substr($this->_data['lastname'], 0, 1));
   }
+
+  public function getFullName ($lastFirst=false) {
+    // Include nickname / middlename along with firstname if present
+    $nick = '';
+    if ($this->_data['nickname']) {
+      $nick = ' (' . $this->_data['nickname'] . ')';
+    }
+    $firstName = $this->_data['firstname'] . $nick . ' ' . $this->_data['middlename'];
+
+    if ($lastFirst) {
+      $r = $this->_data['lastname'] . ', ' . $firstName;
+    } else {
+      $r = $firstName . ' ' . $this->_data['lastname'];
+    }
+
+    return $r;
+  }
 }
