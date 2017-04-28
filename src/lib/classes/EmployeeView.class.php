3e8008106a64cf2e02451269896f134f9d9cf0d3
Scott Haefner <shaefner@usgs.gov>
2017-04-28 14:06:14 -0700

    add method for getting back link; flesh out employee details html; add docs

--- src/lib/classes/EmployeeView.class.php
+++ src/lib/classes/EmployeeView.class.php
@@ -1,8 +1,7 @@
 <?php
 
 /**
- * Employee view
- * - creates the HTML for employee.php
+ * Employee view - creates the HTML for employee.php
  *
  * @author Scott Haefner <shaefner@usgs.gov>
  */
@@ -13,15 +12,65 @@ class EmployeeView {
     $this->_model = $model;
   }
 
-  private function _getEmployee () {
-    $html = '';
+  /**
+   * Get HTML for back link
+   *
+   * @return {String}
+   */
+  private function _getBackLink () {
+    return '<p class="back">&laquo; <a href="../">Back to Staff Directory</a></p>';
+  }
 
-    $html .= $this->_model->email;
+  /**
+   * Get HTML for Employee details table
+   *
+   * @return $html {String}
+   */
+  private function _getEmployee () {
+    $html = '<table>';
+
+    if ($this->_model->phone) {
+      $html .= sprintf('<tr><th>Phone</th><td>%s</td></tr>',
+        $this->_model->phone
+      );
+    }
+    $html .= sprintf('<tr><th>Email</th><td><a href="mailto:%s">%s</a></td></tr>',
+      $this->_model->email,
+      $this->_model->email
+    );
+    if ($this->_model->room) {
+      $html .= sprintf('<tr><th>Room</th><td>%s</td></tr>',
+        $this->_model->room
+      );
+    }
+    $html .= sprintf('<tr><th>Address</th><td>%s</td></tr>',
+      $this->_model->getAddress()
+    );
+    if ($this->_model->webpage) {
+      $html .= sprintf('<tr><th>Web</th><td><a href="%s">%s</a></td></tr>',
+        $this->_model->webpage,
+        $this->_model->webpage
+      );
+    }
+    if ($this->_model->orcid) {
+      $html .= sprintf('<tr><th>ORCid</th><td>
+          <a href="https://orcid.org/%s">https://orcid.org/%s</a>
+        </td></tr>',
+        $this->_model->orcid,
+        $this->_model->orcid
+      );
+    }
+
+    $html .= '</table>';
 
     return $html;
   }
 
+  /**
+   * Render HTML
+   */
   public function render () {
     print $this->_getEmployee();
+    print $this->_getBackLink();
   }
 }
