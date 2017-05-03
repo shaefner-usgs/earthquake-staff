1ca9cf947b758b1112ee9762434518da008de432
Scott Haefner <shaefner@usgs.gov>
2017-05-03 12:16:38 -0700

    add sort method to sort by location

--- src/lib/classes/EmployeeCollection.class.php
+++ src/lib/classes/EmployeeCollection.class.php
@@ -7,9 +7,11 @@
  */
 class EmployeeCollection {
   public $employees;
+  public $sortField;
 
   public function __construct () {
     $this->employees = [];
+    $this->sortField = 'lastname'; // default value
   }
 
   /**
@@ -19,5 +21,28 @@ class EmployeeCollection {
    */
   public function add ($employee) {
     $this->employees[] = $employee;
+  }
+
+  /**
+   * Sort employees
+   *
+   * @param $sortBy {String <lastname | location>}
+   */
+  public function sort ($sortBy) {
+    // array_multisort() requires an array of columns; put sort fields in columns
+    foreach ($this->employees as $index => $Employee) {
+      // Force sort fields to all lowercase to perform case-insensitive sort
+      $firstname[$index] = strtolower($Employee->firstname);
+      $lastname[$index] = strtolower($Employee->lastname);
+      $location[$index] = strtolower($Employee->location);
+    }
+
+    if ($sortBy === 'lastname') {
+      array_multisort($lastname, SORT_ASC, $firstname, SORT_ASC, $this->employees);
+      $this->sortField = $sortBy;
+    } else if ($sortBy === 'location') {
+      array_multisort($location, SORT_ASC, $lastname, SORT_ASC, $this->employees);
+      $this->sortField = $sortBy;
+    }
   }
 }
